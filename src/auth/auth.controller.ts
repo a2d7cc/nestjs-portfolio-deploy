4a698af7ac31e660d515d04b10f9ce128ff9de31
a2d7cc <a2d7cc@gmail.com>
2023-04-05 12:43:15 +0200

    Auth Module Completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthDto } from './dto/auth.dto'
+import { refreshToken } from './dto/refreshToken.dto'
 
 @Controller('auth')
 export class AuthController {
-    
+	constructor(private readonly authService: AuthService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('registration')
+	async registration(@Body() dto: AuthDto) {
+		const user = await this.authService.createUser(dto)
+		const userResponse = await this.authService.buildUserResponse(user)
+		return userResponse
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		const user = await this.authService.login(dto)
+		const userResponse = await this.authService.buildUserResponse(user)
+		return userResponse
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login/access-token')
+	async getNewTokens(@Body() dto: refreshToken) {
+		const user = await this.authService.getNewTokens(dto)
+		const userResponse = await this.authService.buildUserResponse(user)
+		return userResponse
+	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,114 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	UnauthorizedException,
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { InjectRepository } from '@nestjs/typeorm'
+import { compare, genSalt, hash } from 'bcryptjs'
+import { Repository } from 'typeorm'
+import {
+	ALREADY_REGISTRED_ERROR,
+	DONT_HAVE_ACCESS_ERROR,
+	USER_NOT_FOUND_ERROR,
+	WRONG_PASSWORD_ERROR,
+} from './auth.constants'
+import { AuthDto } from './dto/auth.dto'
+import { refreshToken } from './dto/refreshToken.dto'
+import { User } from '../user/entities/user.entity'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>,
+		private readonly jwtService: JwtService
+	) {}
+
+	async createUser({ email, password }: AuthDto): Promise<User> {
+		const existingUser = await this.userRepository.findOne({
+			where: { email },
+		})
+		if (existingUser) {
+			throw new BadRequestException(ALREADY_REGISTRED_ERROR)
+		}
+
+		const salt = await genSalt(5)
+		const hashPassword = await hash(password, salt)
+
+		const user = this.userRepository.create({
+			email,
+			hashPassword,
+		})
+		return await this.userRepository.save(user)
+	}
+
+	async login({ email, password }: AuthDto): Promise<User> {
+		const user = await this.userRepository.findOne({
+			where: { email },
+		})
+		if (!user) {
+			throw new BadRequestException(USER_NOT_FOUND_ERROR)
+		}
+
+		const isValidPass = await compare(password, user.hashPassword)
+		if (!isValidPass) {
+			throw new UnauthorizedException(WRONG_PASSWORD_ERROR)
+		}
+
+		return user
+	}
+
+	async issueTokenPair({ id }: User) {
+		const refreshToken = await this.jwtService.signAsync(
+			{ id },
+			{
+				expiresIn: '15d',
+			}
+		)
+
+		const accessToken = await this.jwtService.signAsync(
+			{ id },
+			{
+				expiresIn: '1h',
+			}
+		)
+
+		return {
+			refreshToken,
+			accessToken,
+		}
+	}
+
+	async buildUserResponse(user: User) {
+		const tokens = await this.issueTokenPair(user)
+
+		return {
+			user,
+			...tokens,
+		}
+	}
+
+	async getNewTokens({ refreshToken }: refreshToken) {
+		if (!refreshToken) {
+			throw new UnauthorizedException(DONT_HAVE_ACCESS_ERROR)
+		}
+
+		const result = await this.jwtService.verifyAsync(refreshToken)
+		
+		if (!result) {
+			throw new UnauthorizedException()
+		}
+		const user = await this.userRepository.findOne({
+			where: { id: result.id },
+		})
+
+		if (!user) {
+			throw new BadRequestException(
+				'User with this payload from JWT not founded'
+			)
+		}
+
+		return user;
+	}
+}
