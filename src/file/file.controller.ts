43ad74fccb5e65af44b93a972e2eee2df6fad09b
a2d7cc <a2d7cc@gmail.com>
2023-04-08 12:09:47 +0200

    Download Resume

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,8 +1,11 @@
 import {
 	Controller,
+	Get,
+	Header,
 	HttpCode,
 	Post,
 	Query,
+	StreamableFile,
 	UploadedFile,
 	UseInterceptors,
 } from '@nestjs/common'
@@ -10,6 +13,8 @@ import { FileInterceptor } from '@nestjs/platform-express'
 import { Auth } from '..//auth/decorators/auth.decorator'
 import { FileElementResponce } from './file.responce'
 import { FileService } from './file.service'
+import path, { join } from 'path'
+import { createReadStream, fstat } from 'fs'
 
 @Controller('files')
 export class FileController {
@@ -25,4 +30,12 @@ export class FileController {
 	): Promise<FileElementResponce[]> {
 		return this.fileService.saveFiles([file], folder)
 	}
+
+	@Get('resume')
+	@Header('Content-Type', 'application/pdf')
+	@Header('Content-Disposition', 'attachment; filename="Resume.pdf"')
+	getStaticFile(): StreamableFile {
+	  const file = createReadStream(join(process.cwd(), 'uploads/resume/Resume.pdf'));
+	  return new StreamableFile(file);
+	}  
 }
